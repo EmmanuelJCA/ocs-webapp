11e25433df001544f1442a4ad771d8f0060aa056
Emmanuel Cañate <emmanuelcanate@gmail.com>
2024-01-22 21:17:03 -0400

    fix: manifest icons path

--- vite.config.ts
+++ vite.config.ts
@@ -20,21 +20,21 @@ export default defineConfig(({ mode }) => {
           background_color: '#ffffff',
           display: 'standalone',
           lang: 'es-ES',
-          start_url: '/favicon/',
+          start_url: '.',
           icons: [
             {
-              src: 'android-chrome-192x192.png',
+              src: 'favicon/android-chrome-192x192.png',
               sizes: '192x192',
               type: 'image/png',
               purpose: 'any',
             },
             {
-              src: 'android-chrome-512x512.png',
+              src: 'favicon/android-chrome-512x512.png',
               sizes: '512x512',
               type: 'image/png',
             },
             {
-              src: 'android-chrome-512x512.png',
+              src: 'favicon/android-chrome-512x512.png',
               sizes: '512x512',
               type: 'image/png',
               purpose: 'any maskable',
