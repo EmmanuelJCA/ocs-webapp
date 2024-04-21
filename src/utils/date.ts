429ef478f8a0715d088632b18f2aee11c9e0e6b1
Emmanuel Cañate <emmanuelcanate@gmail.com>
2024-04-21 19:45:13 -0400

    fix: date format util

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,6 +1,11 @@
 export const formatDate = (date: string | Date, options?: Intl.DateTimeFormatOptions) => {
   const dateToFormat = new Date(date);
-  const formattedDate = dateToFormat.toLocaleDateString('es-ES', {
+
+  const formattedDate = new Date(
+    dateToFormat.getUTCFullYear(),
+    dateToFormat.getUTCMonth(),
+    dateToFormat.getUTCDate()
+  ).toLocaleDateString('es-ES', {
     year: 'numeric',
     month: 'short',
     day: 'numeric',
