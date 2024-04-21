d5d4ddd41129b0ed820bb9c3b0894651adb21573
Emmanuel Cañate <emmanuelcanate@gmail.com>
2024-04-21 19:47:09 -0400

    feat: role traduction

--- src/types/role.ts
+++ src/types/role.ts
@@ -6,3 +6,12 @@ export enum Role {
   NURSE = 'NURSE',
   RADIOTHERAPIST = 'RADIOTHERAPIST',
 }
+
+export const RoleInSpanish: { [key in Role]: string } = {
+  SUPER_ADMINISTRATOR: 'Súper Administrador',
+  ADMINISTRATOR: 'Administrador',
+  ONCOLOGIST: 'Oncólogo',
+  RADIONCOLOGIST: 'Radioncólogo',
+  NURSE: 'Enfermero',
+  RADIOTHERAPIST: 'Radioterapeuta',
+};
